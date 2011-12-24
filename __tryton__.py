818525f68ebf927c3c135d256814e25fb73fa229
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2011-12-24 10:34:38 +0530

    Inital development version

---
+++ __tryton__.py
@@ -0,0 +1,18 @@
+{
+    "name" : "Fleet Management",
+    "version" : "2.2.0.1",
+    "author" : "Sharoon Thomas",
+    'email': 'sharoon.thomas@openlabs.co.in',
+    'website': 'http://www.openlabs.co.in/',
+    "category" : "Generic",
+    "description": "A Module for Fleet Management",
+    "depends" : [
+        "company",
+        "product",
+        "account",
+    ],
+    "xml" : [
+    ],
+    'translation': [
+    ],
+}
